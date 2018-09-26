c3705f5c40943694ea39601ef2b609e331c1c1bd
Andres Castro <andres_fel94@hotmail.com>
2018-09-26 08:13:38 -0500

    pintando de rojo valores salidos del rango

--- control-calidad/consulta-total.php
+++ control-calidad/consulta-total.php
@@ -9,16 +9,16 @@
     <body class="img_fondo" background="../imgs/lab.jpg">    
         <main>
            <div class="topnav">
-                <a class="active" href="../ingresar-resultado/ingresar_resultado.html">Ingresar resultado</a>
-                <a href="consulta_masiva.php">Consultar</a>
-                <a href="#contact">Calidad</a>
+                <a href="../ingresar-resultado/ingresar_resultado.html">Ingresar resultado</a>
+                <a href="../consulta-masiva/consulta_masiva.php">Consultar</a>
+                <a class="active" href="#contact">Calidad</a>
                 <a href="#about">Resultados</a>
                 <a href="index.html">Menu inicial</a>
             </div>
        <br><br>
     <div class="form-hce">
    
-        <h2 class="form__titulo">Pacientes registrados</h2>
+        <h2 class="form__titulo">Registro de control de calidad</h2>
         <br>
         <?php
         include ("../Base_datos/conexion_db.php");
@@ -36,6 +36,26 @@
            <td><b>TRIG lvl 2</b></td>
         </tr>
         <tr>';
+
+        $sql2=$connection->query("SELECT * FROM set_point") or die (mysql_error());
+        $set=$sql2->fetch_array(MYSQLI_NUM);
+        $colmin1 = $set[0];
+        $colmax1 = $set[1];
+        $hdlmin1 = $set[2];
+        $hdlmax1 = $set[3];
+        $ldlmin1 = $set[4];
+        $ldlmax1 = $set[5];
+        $trigmin1 = $set[6];
+        $trigmax1 = $set[7];
+        $colmin2 = $set[8];
+        $colmax2 = $set[9];
+        $hdlmin2 = $set[10];
+        $hdlmax2 = $set[11];
+        $ldlmin2 = $set[12];
+        $ldlmax2 = $set[13];
+        $trigmin2 = $set[14];
+        $trigmax2 = $set[15];
+
         $sql=$connection->query("SELECT * FROM qlty_ctrol") or die (mysql_error());
         while ($resultado = $sql->fetch_array(MYSQLI_NUM)){
             $fecha=$resultado[0];
@@ -46,18 +66,67 @@
             $ldl1=$resultado[5];
             $ldl2=$resultado[6];
             $trig1=$resultado[7];
-            $trig2=$resultado[8];
+            $trig2=$resultado[8];     
             $i=$i+1;
-            echo "<td align='center'>".$fecha."</td>
-            <td>".$col1."</td>
-            <td>".$col2."</td>
-            <td>".$hdl1."</td>
-            <td>".$hdl2."</td>
-            <td>".$ldl1."</td>
-            <td>".$ldl2."</td>
-            <td>".$trig1."</td>
-            <td>".$trig2."</td>
-            </tr>";
+            echo "<td align='center'>".$fecha."</td>";
+
+            if ( ($col1 < $colmin1 ) || ($col1 > $colmax1) ){
+                echo"<td bgcolor='#FF0000' >".$col1."</td>";
+            }
+            else{
+                echo"<td bgcolor='#B9CCDD'>".$col1."</td>";
+            }
+
+            if ( ($col2 < $colmin2 ) || ($col2 > $colmax2) ){
+                echo"<td bgcolor='#FF0000' >".$col2."</td>";
+            }
+            else{
+                echo"<td bgcolor='#B9CCDD'>".$col2."</td>";
+            }
+
+            if ( ($hdl1 < $hdlmin1 ) || ($hdl1 > $hdlmax1) ){
+                echo"<td bgcolor='#FF0000' >".$hdl1."</td>";
+            }
+            else{
+                echo"<td bgcolor='#B9CCDD'>".$hdl1."</td>";
+            }
+
+            if ( ($hdl2 < $hdlmin2 ) || ($hdl2 > $hdlmax2) ){
+                echo"<td bgcolor='#FF0000' >".$hdl2."</td>";
+            }
+            else{
+                echo"<td bgcolor='#B9CCDD'>".$hdl2."</td>";
+            }
+
+            if ( ($ldl1 < $ldlmin1 ) || ($ldl1 > $ldlmax1) ){
+                echo"<td bgcolor='#FF0000' >".$ldl1."</td>";
+            }
+            else{
+                echo"<td bgcolor='#B9CCDD'>".$ldl1."</td>";
+            }
+
+            if ( ($ldl2 < $ldlmin2 ) || ($ldl2 > $ldlmax2) ){
+                echo"<td bgcolor='#FF0000' >".$ldl2."</td>";
+            }
+            else{
+                echo"<td bgcolor='#B9CCDD'>".$ldl2."</td>";
+            }
+
+            if ( ($trig1 < $trigmin1 ) || ($trig1 > $trigmax1) ){
+                echo"<td bgcolor='#FF0000' >".$trig1."</td>";
+            }
+            else{
+                echo"<td bgcolor='#B9CCDD'>".$trig1."</td>";
+            }
+
+            if ( ($trig2 < $trigmin2 ) || ($trig2 > $trigmax2) ){
+                echo"<td bgcolor='#FF0000' >".$trig2."</td>";
+            }
+            else{
+                echo"<td bgcolor='#B9CCDD'>".$trig2."</td>";
+            }
+
+            echo"</tr>";
 
         ?>
         
